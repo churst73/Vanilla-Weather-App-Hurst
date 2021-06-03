ef5e81ca25c12c8880ab2df0f210d51a3ede4d44
churst73 <chelsea.ann.hurst@gmail.com>
2021-06-03 18:02:42 -0400

    Added current location function and unit conversion

--- src/script.js
+++ src/script.js
@@ -42,3 +42,49 @@ search("London");
 
 let form = document.querySelector("#search");
 form.addEventListener("submit", handleSubmit);
+
+function locationName(response) {
+  let apiKey = "90cc04858e590b27514e091504dac169";
+  let city = response.data.name;
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(showTemperature);
+}
+
+function searchLocation(position) {
+  let apiKey = "90cc04858e590b27514e091504dac169";
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+  axios.get(apiUrl).then(locationName);
+}
+
+function locationWeather(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let currentLocation = document.querySelector("#current-location");
+currentLocation.addEventListener("click", locationWeather);
+
+function changeDegree() {
+  let imageCelcius = document.querySelector("#degree-celcius");
+  let loveChange = document.querySelector("#love");
+  if (loveChange.innerHTML.trim() === "Love Fahrenheit?") {
+    imageCelcius.src = "images/degreef.png";
+    let celcius = document.querySelector("#current-celcius");
+    let fahrenheit = celcius.innerHTML;
+    celcius.innerHTML = Math.round((fahrenheit * 9) / 5 + 32);
+    loveChange.innerHTML = "Love Celcius?";
+  } else {
+    imageCelcius.src = "images/degreec.png";
+    let fahrenheit = document.querySelector("#current-celcius");
+    let celcius = fahrenheit.innerHTML;
+    fahrenheit.innerHTML = Math.round(((celcius - 32) * 5) / 9);
+    let loveChange = document.querySelector("#love");
+    loveChange.innerHTML = "Love Fahrenheit?";
+  }
+}
+
+let change = document.querySelector("#degree-change");
+change.addEventListener("click", changeDegree);
