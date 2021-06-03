b8ab5efabc759c99d265360b64bfb87b9eebabc6
churst73 <chelsea.ann.hurst@gmail.com>
2021-06-03 17:40:12 -0400

    Update the time

--- src/script.js
+++ src/script.js
@@ -1,14 +1,29 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function showTemperature(response) {
   let temperatureElement = document.querySelector("#current-celcius");
   let cityElement = document.querySelector("#search-city");
   let descirptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humid");
   let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#current-date");
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descirptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "90cc04858e590b27514e091504dac169";
