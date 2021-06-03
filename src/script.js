41dd2a2b87ab359711d066735617c6254d9a9881
churst73 <chelsea.ann.hurst@gmail.com>
2021-06-03 17:28:00 -0400

    Showing current temperature

--- src/script.js
+++ src/script.js
@@ -1,1 +1,17 @@
+function showTemperature(response) {
+  let temperatureElement = document.querySelector("#current-celcius");
+  let cityElement = document.querySelector("#search-city");
+  let descirptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humid");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descirptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
 
+let apiKey = "90cc04858e590b27514e091504dac169";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(showTemperature);
