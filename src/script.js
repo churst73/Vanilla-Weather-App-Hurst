d132830cbb91b13787d24c6bb18cd59b81ed1949
churst73 <chelsea.ann.hurst@gmail.com>
2021-06-10 14:53:02 -0400

    Made API call

--- src/script.js
+++ src/script.js
@@ -10,7 +10,8 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
@@ -37,6 +38,12 @@ function displayForecast() {
   forecastElement.innerHTML = forecastHTML;
 }
 
+function getForecast(coordinates) {
+  let apiKey = "90cc04858e590b27514e091504dac169";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&unit=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function showTemperature(response) {
   let temperatureElement = document.querySelector("#current-celcius");
   let cityElement = document.querySelector("#search-city");
@@ -56,6 +63,8 @@ function showTemperature(response) {
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
   iconElement.setAttribute("src", `icons/${response.data.weather[0].main}.png`);
   iconElement.setAttribute("alt", response.data.weather[0].main);
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -128,4 +137,3 @@ let change = document.querySelector("#degree-change");
 change.addEventListener("click", changeDegree);
 
 search("London");
-displayForecast();
